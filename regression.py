fbad985a46bf9915654550fe1c0580f49d113792
salmonpakuti <168065430+salmonpakuti@users.noreply.github.com>
2024-05-16 15:16:51 +0900

    4th

---
+++ regression.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class LinearRegression:
+    x = None
+    theta = None
+    y = None
+    
+    def fit(self,x,y):
+        temp = np.linalg.inv(np.dot(x.T,x))
+        self.theta = np.dot(np.dot(temp,x.T),y)
+
+    def predict(self,x):
+        return np.dot(x,self.theta)
+
+    def score(self,x,y):
+        pass
+
+    
